effddee07e338c2217de38c1d5aa89c7e56f7b04
Shaun Lee <shaundlee.0103@gmail.com>
2020-05-13 13:36:43 -0400

    added initial variables and equations necessary to calculate rates

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
-
-
+let principal = 200000;
+let interestRate = 0.05;
+let years = 30;
+let name = "Shaun";
 
 
 // 🏡 Task 1.5: Simple Math
@@ -16,6 +18,8 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 Create another variable called `periods` and give it the value of years*12.
 */
 
+let monthlyInterestRate = interestRate / 12;
+let periods = years * 12;
 
 
 
@@ -29,7 +33,13 @@ Hint #2: you'll need to use the `math` object for parts of this calculation!
 When your math is correct, monthlyRate will equal 1073.64
 */
 
+let compound = Math.pow((1+ monthlyInterestRate), periods);
 
+let numerator = monthlyInterestRate * compound;
+let denominator = compound - 1;
+
+
+let monthlyRate = principal * (numerator / denominator);
 
 
 // 🏡 Task 3: Function
